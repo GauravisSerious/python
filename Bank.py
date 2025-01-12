b3c3bad5b23cb0cf9f105428f614b2393f7c602d
GauravisSerious <gauravjagtap2188@gmail.com>
2025-01-12 21:33:11 +0530

    Update Bank.py

---
+++ Bank.py
@@ -0,0 +1,36 @@
+class Account:
+    def __init__(self,name,balance,min_balance):
+        self.name=name
+        self.balance=balance
+        self.min_balance=min_balance
+    
+    def deposit(self,amount):
+        self.balance+=amount
+
+    def withdraw(self,amount):
+        if self.balance - amount >= self.min_balance:
+            self.balance-=amount
+        else:
+            print("Not enough credits aavailable")
+    
+    def statement(self):
+        print("Account balance:{}".format(self.balance))
+
+class current(Account):
+    def __init__(self, name, balance):
+        super().__init__(name, balance,min_balance = -1000)
+    def __str__(self):
+        return "{}'s current account: Balance ${}".format(self.name,self.balance)
+
+
+class savings(Account):
+    def __init__(self, name, balance):
+        super().__init__(name, balance,min_balance=0)
+        
+
+#USAGE 
+# x = savings("Gaurav",10000)
+# x.deposit(3000)
+# x.statement()
+# x.withdraw(14000)
+# # x.statement()
