bc89a7a2ce3ccacbf1fed8c2e253ec5d3dcc219b
GauravisSerious <gauravjagtap2188@gmail.com>
2025-03-13 12:11:24 +0530

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,11 @@
+def bubble(arr):
+  n = len(arr)
+
+  for i in range(n):
+    swapped = False
+    for j in range(0,n-1-i):
+      if arr[j]>arr[j+1]:
+        arr[j],arr[j+1] = arr[j+1],arr[j]
+        swapped = True
+    if not swapped:
+      break
